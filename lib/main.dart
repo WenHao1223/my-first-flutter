4b6835cd1d9fbc3e38efc479c9f3a367ed7eb2d0
Lim Wen Hao <wenhaojshs@gmail.com>
2024-07-13 23:01:15 +0800

    Prettier the app

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,12 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'My First Flutter',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          // colorScheme: ColorScheme.fromSeed(seedColor: Color.fromRGBO(0, 255, 0, 1.0)),
+          // colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF00FF00)),
         ),
         home: MyHomePage(),
       ),
@@ -39,22 +41,60 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // center the ui
+          children: [
+            // Text('A random AWESOME idea:'),
+            // Text(appState.current.asLowerCase),
+            
+            BigCard(pair: pair),
 
-          // Add a button
-          ElevatedButton(
-            onPressed: () {
-              // print('button pressed!');
-              appState.getNext();
-            },
-            child: Text('Next')
-          ),
-        ],
+            // add separation between two widgets
+            SizedBox(height: 10),
+        
+            // Add a button
+            ElevatedButton(
+              onPressed: () {
+                // print('button pressed!');
+                appState.getNext();
+              },
+              child: Text('Next')
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context); // add theme
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary, // add theme color
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
